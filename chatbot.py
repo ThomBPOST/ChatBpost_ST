69236be35c626c3ef2e15361d04a78733bb8e444
Tkarsk <thomas.karskens@bpost.be>
2024-05-03 12:23:07 +0200

    adding secrets

--- chatbot.py
+++ chatbot.py
@@ -46,7 +46,7 @@ from translation import get_translation
 
 # Debug Tool 
 
-from debug_tool import show_debug_info
+from debug_tool import sho>w_debug_info
 
 msgs = StreamlitChatMessageHistory(key = 'chat_message_history')
 
@@ -70,12 +70,21 @@ show_debug_info(debug_button, msgs)
 
 # chatMistral = ChatMistralAI(mistral_api_key=mistral_api_key)
 
+prompt =(
+        "Tu es un assistant pour les team leaders chez bpost pendant la période des élections. "
+        "Réponds à la question de l'utilisateur en utilisant les informations qui te sont fournies. "
+        "Si tu ne trouves pas les informations, dites-le à l'utilisateur et que tu es là pour répondre aux questions "
+        "sur les process de bpost pour les élections en Belgique. Essaye de répondre un maximum sur base des documents "
+        "que je t'ai fourni et n'invente pas de nouveaux éléments. Répond toujours dans la langue de la question de l'utilisateur. "
+        "Lorsqu'on te pose une question sur un process, je veux que tu mettes TOUTES les étapes dans la réponse, avec les marches à suivre et les remarques importantes si il y en a, de manière structurée avec des retours à la lignes si il faut. "
+        "Quand on te demande la définition ou la signification d'un mot, répond d'un phrase courte et simple, et ou d'exemples. Donne juste la signification du mot."
+        " Si tu reçois un message qui n'a aucun sens, répond simplement : Je ne comprend pas votre question ." 
+        " Si le 'type': 'Lexique', alors répond de la manière la plus brève possible en restant proche de la définition du document."
+        " Si le 'type': 'SOP', alors répond en donnant les étapes du documents. Il faut que la personne comprennent toutes les étapes "
+        " Si le 'type': 'Text', je veux que tu répondes de la manière la plus brève possible en maximum 2 ou 3 phrases. "
+)
 
 
-
-
-
-#interface streamlit online 
 
 with st.sidebar:
     # Demander la clé API à l'utilisateur
@@ -105,7 +114,7 @@ with st.sidebar:
             )
 
             retriever = vector_store.as_retriever()
-            
+
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
